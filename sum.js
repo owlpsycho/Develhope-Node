bc78c7fa784c354c291375c7022d5c6ba68e3dbc
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-13 14:24:25 +0100

    risoluzione esercizio 01

--- sum.js
+++ sum.js
@@ -1,7 +1,4 @@
-// Define two numbers
-const number1 = 20;
-const number2 = 8;
-
-const sum = number1 + number2;
-
-console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
+const sum = (num1, num2) => {
+    return num1 + num2
+}
+console.log(sum(20, 8));
