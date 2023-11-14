1a25d0151e6521146483932effa94e23f6dcc2ee
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-14 13:18:34 +0100

    server

---
+++ server.mjs
@@ -0,0 +1,16 @@
+// Creazione server
+import { createServer } from "node:http"
+
+const server = createServer((request, response) => {
+    console.log("request recived")
+
+    response.statusCode = 200
+
+    response.setHeader("Content-Type", "text/html")
+
+    response.end("<html><body><h1>This page was served with Node.js!</h1></body></html>")
+})
+
+server.listen(3000, ()=>{
+    console.log("Server running at http://localhost:3000")
+})
