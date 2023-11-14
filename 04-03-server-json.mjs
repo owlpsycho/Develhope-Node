7d09d22c857a1db77b09d1c324cc1a0ea786de0a
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-14 13:48:45 +0100

    risoluzione esercizio 06

---
+++ 04-03-server-json.mjs
@@ -0,0 +1,19 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
+
+// se metto Mars il valore di Content-Length è 19 con Earth è 20
