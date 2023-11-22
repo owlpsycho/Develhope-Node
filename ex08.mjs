5a644dba126c3944f945e9caef9f1b0907ca848c
Denis Bruno <denisbruno.DB28@gmail.com>
2023-11-22 18:14:42 +0100

    risoluzione esercizio 08

--- ex08.mjs
+++ ex08.mjs
@@ -3,5 +3,10 @@ import * as fs from "node:fs";
 fs.writeFile("prova.txt", "Hello, World!", (err) => {
   if (err) {
     console.error(err);
-  } 
+  }
+  fs.writeFile("prova2.txt", "Hello, World 2!", (err) => {
+    if (err) {
+      console.error(err);
+    } 
+  }); 
 });
