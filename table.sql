4562e550c49660ce0ba682217e52f63943bb3c76
Denis Bruno <denisbruno.DB28@gmail.com>
2023-12-18 15:44:23 +0100

    risoluzione esercizio 1

---
+++ table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    genre VARCHAR,
+    published_year INT,
+    isbn VARCHAR UNIQUE,
+    price DECIMAL,
+    rating FLOAT,
+    stock_count INT
+);
